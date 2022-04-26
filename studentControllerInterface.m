ba1b793a3a2be989b2155aded1b34daaa22f6bad
Fung <koufongso@gmail.com>
2022-04-26 11:30:48 -0700

    modified for simulink

--- studentControllerInterface.m
+++ studentControllerInterface.m
@@ -1,39 +1,46 @@
 classdef studentControllerInterface < matlab.System
     properties (Access = private)
-        %% You can add values that you want to store and updae while running your controller.
-        % For more information of the supported data type, see
-        % https://www.mathworks.com/help/simulink/ug/data-types-supported-by-simulink.html
         t_prev = -1;
         theta_d = 0;
         p_ball_prev = nan;
         theta_prev = nan;
+        V_servo_prev = 0;
+    end
 
-        % param
+    properties(Constant)
+        % sys param
         g = 9.81;
         r_arm = 0.0254;
         L = 0.4255;
-        
+
         a = 5 * 9.81 * 0.0254 / (7 * 0.4255);
         b = (5 * 0.4255 / 14) * (0.0254 / 0.4255)^2;
         c = (5 / 7) * (0.0254 / 0.4255)^2;
-    
+            
         K = 10;
         tau = 0.1;
 
-        % cost weight
-        w_x = 1800;
-        w_u = 5;
+        % MPC param
+        T = 1;                    % [s] time horizon
+        dt_mpc = 0.1;             % [s] time step
+        N = 10;                   % total step size
+        lb = repmat([-0.19;-Inf;-1.047;-Inf;-10],10,1);
+        ub = repmat([0.19;Inf;1.047;Inf;10],10,1);
+
+        Bt=[0;0;0;100];     %Bt=[0;0;0;K/tau];
+        % cost weight 
+        H = kron(eye(10),diag([1800,0,0,0,5])); % kron(eye(N),diag([1800,0,0,0,5]))
     end
 
     %% MPC controller
     methods(Access = protected)
-        function V_servo = mpcImpl(obj, t, p_ball, theta)
+        function V_servo = stepImpl(obj, t, p_ball, theta)
+            % some often-used variable
+            n = obj.N;
+            M = (4+1)*n;
+            n_ = n-1;
 
-            T = 2;              % [s] time horizon
-            dt_mpc = 0.1;       % [s] time step cannot be too small
-            N = T/dt_mpc;       % total step size
-            
-            [p_ball_ref, ~, ~] = get_ref_traj(t:dt_mpc:t+T); % obtian reference in the time window
+            [p_ball_ref, ~, ~] = get_ref_traj(t:obj.dt_mpc:t+obj.T); % obtian reference in the time window
             
             % x1: p_ball
             % x2: p_ball_dot
@@ -48,6 +55,11 @@ classdef studentControllerInterface < matlab.System
                 theta_dot = 0;
             else
                 dt = t - obj.t_prev;
+                % some quick fix for t2 and t3 are the same for some unknown reason
+                if(dt==0)
+                    V_servo = obj.V_servo_prev;
+                    return
+                end
                 p_ball_dot = (p_ball-obj.p_ball_prev)/dt;
                 theta_dot = (theta-obj.theta_prev) /dt;
             end
@@ -59,53 +71,58 @@ classdef studentControllerInterface < matlab.System
             At=[0,1,0,0;
                 obj.c*x0(4)^2*cos(x0(3))^2,0,obj.a*cos(x0(3))+d*x0(4)^2*sin(2*x0(3)),-2*d*cos(x0(3))^2*x0(4);
                 0,0,0,1;
-                0,0,0,-1/obj.tau];
-            Bt=[0;0;0;obj.K/obj.tau];
+                0,0,0,-10];
             
             % construct matrices for the quadprog solver
-            M = (4+1)*N;
-            lb = repmat([-0.19;-inf;-1.047;-inf;-10],N,1);
-            ub = repmat([0.19;inf;1.047;inf;10],N,1);
-            
-            S = [eye(4)+At*dt_mpc,Bt*dt_mpc];
-            SS = repmat({S}, 1, N-1);
-            Aeq1 = blkdiag(SS{:});
+            S = [eye(4)+At*obj.dt_mpc,obj.Bt*obj.dt_mpc];
+            Aeq1 =kron(eye(n_),S);
             
             I = [-eye(4),zeros(4,1)];
-            II = repmat({I}, 1, N-1);
-            Aeq2 = blkdiag(II{:});
+            Aeq2 = kron(eye(n_),I);
             
-            Aeq = zeros(4*N,M);
-            Aeq(1:4*(N-1),1:(N-1)*5) = Aeq1;
-            Aeq(1:4*(N-1),6:M) = Aeq(1:4*(N-1),6:M)+Aeq2;
-            Aeq(4*(N-1)+1:end,1:5) = -I;
-            beq = zeros(4*N,1);
-            beq(4*(N-1)+1:end) = x0;
+            Aeq = zeros(4*n,M);
+            Aeq(1:4*(n_),1:(n_)*5) = Aeq1;
+            Aeq(1:4*(n_),6:M) = Aeq(1:4*(n_),6:M)+Aeq2;
+            Aeq(4*(n_)+1:end,1:5) = -I;
+            beq = zeros(4*n,1);
+            beq(4*(n_)+1:end) = x0;
             
-            w = diag([obj.w_x,0,0,0,obj.w_u]);
-            ww = repmat({w}, 1, N);
-            H = blkdiag(ww{:});
             ref_vec = zeros(M,1);
-            for i=1:N
+            for i=1:n
                ref_vec((i-1)*5+1) = p_ball_ref(i);
             end
-            f = -ref_vec'*H';
-            options = optimoptions('quadprog','Display','off');
-            x = quadprog(H,f,[],[],Aeq,beq,lb,ub,[],options);
-            V_servo = x(5);
+            f = -ref_vec'*obj.H';
+            xx0 = repmat([x0;0],n,1); % intial guess
+
+%             more accurate inital guess
+%             AA = zeros(5*N,5);
+%             AA(1:4,1:4) = eye(4);
+%             for i=2:N
+%                 AA(5*i-4:5*i-1,1:4)=mpower(At,i-1);
+%             end
+%             xx0 = AA*[x0;0];
+
+            coder.extrinsic('quadprog');
+            options = optimoptions('quadprog','algorithm','active-set','Display','off');
+            x = quadprog(obj.H,f,[],[],Aeq,beq,obj.lb,obj.ub,xx0,options);
+            V_servo = zeros(1,1);
+            if(isnan(x))
+                V_servo = obj.V_servo_prev;
+            else
+                V_servo = x(5);
+            end
 
             % update old states
             obj.t_prev =t;
             obj.p_ball_prev = p_ball;
             obj.theta_prev = theta;
+            obj.V_servo_prev = V_servo;
         end
     end 
 
     methods(Access = public)
-        % Used this for matlab simulation script. fill free to modify it as
-        % however you want.
         function [V_servo, theta_d] = stepController(obj, t, p_ball, theta)        
-            V_servo = mpcImpl(obj, t, p_ball, theta);
+            V_servo = stepImpl(obj, t, p_ball, theta);
             theta_d = obj.theta_d;
         end
     end
