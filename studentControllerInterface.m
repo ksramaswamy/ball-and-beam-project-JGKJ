fbb349581e50f58c5a90c43e9df6e5acae4e754a
ksramaswamy <100734142+ksramaswamy@users.noreply.github.com>
2022-04-25 17:16:36 -0700

    Made some changes for friction and gravity

--- studentControllerInterface.m
+++ studentControllerInterface.m
@@ -1,29 +1,17 @@
 classdef studentControllerInterface < matlab.System
     properties (Access = private)
-        %% You can add values that you want to store and updae while running your controller.
-        % For more information of the supported data type, see
-        % https://www.mathworks.com/help/simulink/ug/data-types-supported-by-simulink.html
+        %% Class Variables
         t_prev = -1;
         theta_d = 0;
-        p_ball_prev = nan;
-        theta_prev = nan;
+        thetaLast = 0;
+        p_ballLast = 0;
+        dt = .01;
+        v_ball = 0;
+        dtheta = 0;
+        controllerType = 2;
         inputLast = 0;
-
-        % param
-        g = 9.81;
-        r_arm = 0.0254;
-        L = 0.4255;
-        
-        a = 5 * 9.81 * 0.0254 / (7 * 0.4255);
-        b = (5 * 0.4255 / 14) * (0.0254 / 0.4255)^2;
-        c = (5 / 7) * (0.0254 / 0.4255)^2;
-    
-        K = 10;
-        tau = 0.1;
-
-        % cost weight
-        w_x = 1800;
-        w_u = 5;
+        GravComp = .5;
+        FrictionComp = .4;
 
         % Notes on Kalman Filter: There's an extra state to account for
         % potential offset on the servo angle measurements. This could
@@ -34,151 +22,166 @@ classdef studentControllerInterface < matlab.System
 
         % Kalman Filter Variables
         Pm = diag([.05 .02 .08 .04 .02]);
-        xm = [-.19 0 0 0 0]';
+        xm = [-.19 0 -50*pi/180 0 0]';
         n = 5;
-        Evv = diag([.01 .01 .01 .01 0.01]).^2;
+        Evv = diag([.04 .01 .03 .01 0.01]).^2;
         Eww = diag([.001 .001]).^2;
+
     end
-
-    %% MPC controller
     methods(Access = protected)
-        function V_servo = mpcImpl(obj, t, p_ball, theta)
+        %         function setupImpl(obj)
+        %             disp("You can use this function for initializaition.")
+        %         end
 
-            T = 2;              % [s] time horizon
-            dt_mpc = 0.1;       % [s] time step cannot be too small
-            N = T/dt_mpc;       % total step size
+        function V_servo = stepImpl(obj, t, p_ball, theta)
+            % This is the main function called every iteration. You have to implement
+            % the controller in this function, but you are not allowed to
+            % change the signature of this function.
+            % Input arguments:
+            %   t: current time
+            %   p_ball: position of the ball provided by the ball position sensor (m)
+            %
+            %   theta: servo motor angle provided by the encoder of the motor (rad)
+            % Output:
+            %   V_servo: voltage to the servo input.
+
+            %% Read data
+            obj.dt = t - obj.t_prev;
+
+            % Extract reference trajectory at the current timestep.
+            [p_ball_ref, v_ball_ref, ~] = get_ref_traj(t);
             
-            [p_ball_ref, ~, ~] = get_ref_traj(t:dt_mpc:t+T); % obtian reference in the time window
+            %% State Estimation - Unscented Kalman Filter
+            %Estimate ball velocity
+            obj.v_ball = (p_ball - obj.p_ballLast)/obj.dt;
+            % Estimate angular velocity of servo
+            obj.dtheta = (theta - obj.thetaLast)/obj.dt;
+            % Generate sigma points
+%             S = chol(obj.n*obj.Pm);
+%             s = zeros(5,10);
+%             for i = 1:5
+%                 s(:,i) = obj.xm + S(:,i);
+%                 s(:,5+i) = obj.xm - S(:,i);
+%             end
+%             % Calculate prior sigma points
+%             sp = zeros(5,10);
+%             for i = 1:obj.n*2
+%                 sp(:,i) = s(:,i) + obj.dynamics(s(:,i),obj.inputLast)*obj.dt;
+%             end
+%             % Compute the prior statistics
+%             xp = mean(sp,2);
+%             Pp = zeros(5);
+%             for i = 1:obj.n*2
+%                 Pp = Pp + 1/(2*obj.n)*(sp(:,i) - xp)*(sp(:,i)-xp)' + obj.Evv;
+%             end
+% 
+%             % Measurement Update
+%             % Sigma points for measurements
+%             sz = [sp(1,:);sp(3,:)+sp(5,:)];
+% 
+%             % Measurement Statistics
+%             zbar = mean(sz,2);
+%             Pzz = zeros(2);
+%             for i = 1:obj.n*2
+%                 Pzz = Pzz + 1/(2*obj.n)*(sz(:,i) - zbar)*(sz(:,i)-zbar)' + obj.Eww;
+%             end
+% 
+%             Pxz = zeros(5,2);
+%             for i = 1:obj.n*2
+%                 Pxz = Pxz + 1/(2*obj.n)*(sp(:,i) - xp)*(sz(:,i)-zbar)';
+%             end
+% 
+%             % Apply Kalman Gain
+%             K = Pxz/Pzz;
+%             obj.xm = xp + K*([p_ball;theta] - zbar);
+%             obj.Pm = Pp - K*Pzz*K';
+% 
+%             % Store data in variables for control
+%             p_ball = obj.xm(1);
+%             obj.v_ball = obj.xm(2);
+%             theta = obj.xm(3);
+%             obj.dtheta = obj.xm(4);
+
+            switch(obj.controllerType)
+                case 1
+                    %% PD Controller
+                    % Decide desired servo angle based on simple proportional feedback.
+                    k_p = 2; k_d = 20;
+                    obj.theta_d = - k_p * (p_ball - p_ball_ref) - k_d*obj.v_ball;
+
+                    % Make sure that the desired servo angle does not exceed the physical
+                    % limit. This part of code is not necessary but highly recommended
+                    % because it addresses the actual physical limit of the servo motor.
+                    theta_saturation = 56 * pi / 180;
+                    obj.theta_d = min(obj.theta_d, theta_saturation);
+                    obj.theta_d = max(obj.theta_d, -theta_saturation);
+
+                    % Simple position control to control servo angle to the desired
+                    % position.
+                    k_servo = 5; kd_servo = 1;
+                    V_servoVirtual = k_servo * (obj.theta_d - theta) - kd_servo*obj.dtheta;
+                case 2
+                    %% Sontag Controller
+                    x1 = p_ball - (p_ball_ref);
+                    x2 = obj.v_ball - (v_ball_ref);
+                    x3 = theta;
+                    x4 = obj.dtheta;
+
+                    V_servoVirtual = ...
+-(1.0*((0.418*sin(x3) + x4^2*cos(x3)^2*(0.00255*x1 - 5.42e-4))*(1633.0*x1 + 1799.0*x2 + 158.0*x3 + 3.52*x4) + x2*(2577.0*x1 + 1633.0*x2 + 125.0*x3 + 2.74*x4) + x4*(125.0*x1 + 158.0*x2 + 41.3*x3 + 0.934*x4) - 40.0*x4*(2.74*x1 + 3.52*x2 + 0.934*x3 + 0.0218*x4) + ((110.0*x1 + 141.0*x2 + 37.4*x3 + 0.871*x4)^4 + ((0.418*sin(x3) + x4^2*cos(x3)^2*(0.00255*x1 - 5.42e-4))*(1633.0*x1 + 1799.0*x2 + 158.0*x3 + 3.52*x4) + x2*(2577.0*x1 + 1633.0*x2 + 125.0*x3 + 2.74*x4) + x4*(125.0*x1 + 158.0*x2 + 41.3*x3 + 0.934*x4) - 40.0*x4*(2.74*x1 + 3.52*x2 + 0.934*x3 + 0.0218*x4))^2)^(1/2)))/(110.0*x1 + 141.0*x2 + 37.4*x3 + 0.871*x4);
+
+                    % Saturate output to conserve energy
+                    saturate = 1;
+                    V_servoVirtual = max(min(V_servoVirtual,saturate),-saturate);
+                case 3
+                    %% LQR Controller
+                    K = [31.6228   37.6022    9.3513    0.2114];
+                    x1 = p_ball - (p_ball_ref);
+                    x2 = obj.v_ball - (v_ball_ref);
+                    x3 = theta;
+                    x4 = obj.dtheta;
+
+                    V_servoVirtual = -K*[x1;x2;x3;x4];
+                    % Saturate output to conserve energy
+                   
+                    saturate = 10;
+                    V_servoVirtual = max(min(V_servoVirtual,saturate),-saturate);
+
+                    
+
+                otherwise
+                    V_servoVirtual = 0;
+            end
             
-            % x1: p_ball
-            % x2: p_ball_dot
-            % x3: theta
-            % x4: theta_dot
             
-            % get current (initial) state, compute dz and dtheta (maybe use
-            % some estimator to have a better approximation)
-            
-            if 1
-                if(obj.t_prev==-1)
-                    % just started, assume 0 accel
-                    p_ball_dot = 0;
-                    theta_dot = 0;
-                else
-                    dt = t - obj.t_prev;
-                    p_ball_dot = (p_ball-obj.p_ball_prev)/dt;
-                    theta_dot = (theta-obj.theta_prev) /dt;
-                end
-                x0 = [p_ball;p_ball_dot;theta;theta_dot];
+            if V_servoVirtual >= 0 && V_servoVirtual <= 5
+                V_servo = V_servoVirtual + obj.GravComp + obj.FrictionComp;
+            elseif V_servoVirtual < 0
+                V_servo = V_servoVirtual + obj.GravComp - obj.FrictionComp;
             else
-                % Unscented Kalman Filter
-                dt = t - obj.t_prev;
-                S = chol(obj.n*obj.Pm);
-                s = zeros(5,10);
-                for i = 1:5
-                    s(:,i) = obj.xm + S(:,i);
-                    s(:,5+i) = obj.xm - S(:,i);
-                end
-                % Calculate prior sigma points
-                sp = zeros(5,10);
-                for i = 1:obj.n*2
-                    sp(:,i) = s(:,i) + obj.dynamics(s(:,i),obj.inputLast)*dt;
-                end
-                % Compute the prior statistics
-                xp = mean(sp,2);
-                Pp = zeros(5);
-                for i = 1:obj.n*2
-                    Pp = Pp + 1/(2*obj.n)*(sp(:,i) - xp)*(sp(:,i)-xp)' + obj.Evv;
-                end
-
-                % Measurement Update
-                % Sigma points for measurements
-                sz = [sp(1,:);sp(3,:)+sp(5,:)];
-
-                % Measurement Statistics
-                zbar = mean(sz,2);
-                Pzz = zeros(2);
-                for i = 1:obj.n*2
-                    Pzz = Pzz + 1/(2*obj.n)*(sz(:,i) - zbar)*(sz(:,i)-zbar)' + obj.Eww;
-                end
-
-                Pxz = zeros(5,2);
-                for i = 1:obj.n*2
-                    Pxz = Pxz + 1/(2*obj.n)*(sp(:,i) - xp)*(sz(:,i)-zbar)';
-                end
-
-                % Apply Kalman Gain
-                Kf = Pxz/Pzz;
-                obj.xm = xp + Kf*([p_ball;theta] - zbar);
-                obj.Pm = Pp - Kf*Pzz*Kf';
-
-                % Store data in variables for control
-                x0 = obj.xm(1:4);
+                V_servo = 5;
             end
-        
-            % MPC
-            % linearized (Jacobian) dynmics system
-            d = obj.c*(obj.L/2-x0(1));
-            At=[0,1,0,0;
-                obj.c*x0(4)^2*cos(x0(3))^2,0,obj.a*cos(x0(3))+d*x0(4)^2*sin(2*x0(3)),-2*d*cos(x0(3))^2*x0(4);
-                0,0,0,1;
-                0,0,0,-1/obj.tau];
-            Bt=[0;0;0;obj.K/obj.tau];
-            
-            % construct matrices for the quadprog solver
-            M = (4+1)*N;
-            lb = repmat([-0.19;-inf;-1.047;-inf;-10],N,1);
-            ub = repmat([0.19;inf;1.047;inf;10],N,1);
-            
-            S = [eye(4)+At*dt_mpc,Bt*dt_mpc];
-            SS = repmat({S}, 1, N-1);
-            Aeq1 = blkdiag(SS{:});
-            
-            I = [-eye(4),zeros(4,1)];
-            II = repmat({I}, 1, N-1);
-            Aeq2 = blkdiag(II{:});
-            
-            Aeq = zeros(4*N,M);
-            Aeq(1:4*(N-1),1:(N-1)*5) = Aeq1;
-            Aeq(1:4*(N-1),6:M) = Aeq(1:4*(N-1),6:M)+Aeq2;
-            Aeq(4*(N-1)+1:end,1:5) = -I;
-            beq = zeros(4*N,1);
-            beq(4*(N-1)+1:end) = x0;
-            
-            w = diag([obj.w_x,0,0,0,obj.w_u]);
-            ww = repmat({w}, 1, N);
-            H = blkdiag(ww{:});
-            ref_vec = zeros(M,1);
-            for i=1:N
-               ref_vec((i-1)*5+1) = p_ball_ref(i);
-            end
-            f = -ref_vec'*H';
-            options = optimoptions('quadprog','Display','off');
-            x = quadprog(H,f,[],[],Aeq,beq,lb,ub,[],options);
+                    
 
-            if ~isempty(x)
-                V_servo = x(5);
-            else
-                V_servo = 0;
-            end
-
-            % update old states
-            obj.t_prev =t;
-            obj.p_ball_prev = p_ball;
-            obj.theta_prev = theta;
-            obj.inputLast = V_servo;
+            %% Update Class Properties
+            obj.t_prev = t;
+            obj.thetaLast = theta;
+            obj.p_ballLast = p_ball;
+            obj.inputLast = V_servoVirtual;
         end
-    end 
+    end
 
     methods(Access = public)
         % Used this for matlab simulation script. fill free to modify it as
         % however you want.
         function [V_servo, theta_d,v_ball,dtheta] = stepController(obj, t, p_ball, theta)
-            V_servo = mpcImpl(obj, t, p_ball, theta);
+            V_servo = stepImpl(obj, t, p_ball, theta);
             theta_d = obj.theta_d;
-            v_ball = 0;
-            dtheta = 0;
+            v_ball = obj.v_ball;
+            dtheta = obj.dtheta;
         end
     end
+
     methods(Access = private)
         function dx = dynamics(~,x,u)
             x1 = x(1);
@@ -206,5 +209,5 @@ classdef studentControllerInterface < matlab.System
             dx(5) = 0;
         end
     end
-    
+
 end
